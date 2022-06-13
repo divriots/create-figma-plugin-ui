7d5a8139fa3c157b5e33521c43b3b7a553320a13
Francois Valdy <604263+undefined@users.noreply.github.com>
2022-06-13 15:07:10 +0000

    remove helpLink

--- src/components/text/stories/text.stories.tsx
+++ src/components/text/stories/text.stories.tsx
@@ -1,47 +1,43 @@
 /** @jsx h */
-import { h } from 'preact'
+import { h } from 'preact';
 
-import { Text } from '../text'
+import { Text } from '../text';
 
 export default {
-  title: 'Components/Text'
-}
+  title: 'Components/Text',
+};
 
 export const Default = function () {
-  return <Text>Text</Text>
-}
+  return <Text>Text</Text>;
+};
 
 export const Numeric = function () {
-  return <Text numeric>3.142</Text>
-}
+  return <Text numeric>3.142</Text>;
+};
 
 export const AlignLeft = function () {
-  const style = { backgroundColor: 'var(--figma-color-bg-brand-tertiary)' }
+  const style = { backgroundColor: 'var(--figma-color-bg-brand-tertiary)' };
   return (
     <div style={style}>
       <Text align="left">Text</Text>
     </div>
-  )
-}
+  );
+};
 
 export const AlignCenter = function () {
-  const style = { backgroundColor: 'var(--figma-color-bg-brand-tertiary)' }
+  const style = { backgroundColor: 'var(--figma-color-bg-brand-tertiary)' };
   return (
     <div style={style}>
       <Text align="center">Text</Text>
     </div>
-  )
-}
+  );
+};
 
 export const AlignRight = function () {
-  const style = { backgroundColor: 'var(--figma-color-bg-brand-tertiary)' }
+  const style = { backgroundColor: 'var(--figma-color-bg-brand-tertiary)' };
   return (
     <div style={style}>
       <Text align="right">Text</Text>
     </div>
-  )
-}
-
-export const WithHelp = function () {
-  return <Text helplink="#">Need help</Text>
-}
+  );
+};
